af56444cfe0ad0c682a19f2d854a62d4e73c971c
vara2005 <muthyalavarareddy@gmail.com>
2025-05-25 12:07:16 +0530

    commit1

---
+++ Task1.js
@@ -0,0 +1,3 @@
+document.getElementById('welcomeButton').addEventListener('click', function() {
+    window.alert('Welcome to Delhi - The city of heritage, culture, and delicious food!\n\nDelhi has been continuously inhabited since the 6th century BCE!');
+});
